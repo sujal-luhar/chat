4f18f6664f46181524767314ecc7c2f1a23d43be
sujal-luhar <luharsujal2712@gmail.com>
2024-08-17 03:15:42 +0530

    Applied generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,42 +1,28 @@
-from django.shortcuts import render, get_object_or_404
 from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
-# Create your views here.
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.views import generic
+
 from .models import Question, Choice
-from django.template import loader
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#     template = loader.get_template("polls/index.html")
-#     context = {
-#         "latest_question_list": latest_question_list
-#     }
-#     return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def index(request):
-    """
-    The shortcut
-    """
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = { "latest_question_list": latest_question_list }
-    return render(request, "polls/index.html", context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
-# def detail(request, question_id):
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exist!")
-#     return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-def detail(request, question_id):
-    question = get_object_or_404(Question , pk=question_id)
-    return render(request, "polls/detail.html", {"question": question}) 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -62,3 +48,43 @@ def vote(request, question_id):
         # reverse("polls:results", args=(question.id,)) -> "/polls/3/results/"
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
+# from django.db.models import F
+# from django.shortcuts import render, get_object_or_404
+# from django.urls import reverse
+
+# # Create your views here.
+# from django.http import HttpResponse, Http404, HttpResponseRedirect
+# from django.template import loader
+
+# from .models import Question, Choice
+
+# # def index(request):
+# #     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+# #     template = loader.get_template("polls/index.html")
+# #     context = {
+# #         "latest_question_list": latest_question_list
+# #     }
+# #     return HttpResponse(template.render(context, request))
+
+# def index(request):
+#     """
+#     The shortcut
+#     """
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = { "latest_question_list": latest_question_list }
+#     return render(request, "polls/index.html", context)
+
+# # def detail(request, question_id):
+# #     try:
+# #         question = Question.objects.get(pk=question_id)
+# #     except Question.DoesNotExist:
+# #         raise Http404("Question does not exist!")
+# #     return render(request, "polls/detail.html", {"question": question})
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question , pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question}) 
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
